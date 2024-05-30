392f07c70b1fabc897a696c4925c67a4cfa3c7ac
heet2306 <heetmodh2306@gmail.com>
2024-05-30 18:43:44 +0530

    first commit

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,6 +1,7 @@
 const { Router } = require("express")
 const { handlePost, handleGet, handleUpdate, handlDelete, getLoginPage, getSignUp, handleLogin } = require("../controllers/userController")
 const passport = require("passport")
+const { isExists } = require("../middlewares/Validate")
 const userRoute = Router()
 userRoute.get("/", handleGet)
 userRoute.post("/", handlePost)
@@ -9,9 +10,12 @@ userRoute.delete("/:id", handlDelete)
 userRoute.get("/login", getLoginPage)
 userRoute.post("/login", handleLogin)
 userRoute.post("/passportLogin", passport.authenticate("local"), (req, res) => {
+    console.log("req.user: " ,req.user);
     res.send("logged in");
 });
-
+userRoute.get("/admin",isExists,(req,res) => {
+res.send({user:req.user,msg:"Welcome"});
+});
 userRoute.get("/signup", getSignUp)
 
 module.exports = userRoute;
---
+++ middlewares/Validate.js
@@ -0,0 +1,9 @@
+const isExists=(req,res,next)=>{
+    if(req.user){
+        return next();
+    }
+    else{
+        res.redirect('/user/login')
+    }
+}
+module.exports={isExists}
